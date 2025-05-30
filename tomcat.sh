5c5ca04b1fdb851bafc39686857c85ae11a073b7
Protalic <nagarajkamathglowtouch@gmail.com>
2025-05-30 21:39:51 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -20,7 +20,7 @@ if ! java -version 2>&1 | grep -q "17"; then
     echo "This may take some time as necessary packages getting installed. Please wait..."
     if ! sudo yum install java-17-openjdk-devel -y &> /dev/null; then
         sudo yum update -y &> /dev/null;sudo yum install wget -y &> /dev/null;sudo mkdir /opt/java-17 &> /dev/null; wget https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz &> /dev/null;sudo tar xf openjdk-17_linux-x64_bin.tar.gz -C /opt/java-17/ --strip-components=1 &> /dev/null;export JAVA_HOME=/opt/java-17 &> /dev/null;export PATH=$JAVA_HOME/bin:$PATH &> /dev/null;java --version
-        fi
+    fi
 else
     echo "Java 17 is already installed." &> /dev/null
 fi
